95b38f5fd930f33549a62ef9469e6f4316e421be
chirag <70309029+chirag486a@users.noreply.github.com>
2025-01-02 16:34:24 +0545

    Add Product Description Model Window Details

--- src/script.js
+++ src/script.js
@@ -66,7 +66,145 @@ const productElString = `
 </div>
 `;
 
+const productDescriptionHtml = `
+<div class="absolute top-0 z-10 p-12 transition-all duration-500 -translate-x-1/2 bg-white border-4 left-1/2 w-fit h-fit border-slate-900 product-description-model">
+  <div>
+    <h3 class="mb-4 text-2xl">Product Details</h3>
+    <div class="mb-12">
+      <div class="flex gap-8">
+        <div class="flex flex-col gap-2">
+          <div class="bg-black min-w-80 aspect-square product-image-container relative">
+            <img src="" alt="" class="text-center transition-all duration-150 aspect-square product-image blur-sm object-cover">
+            <p class="absolute inset-0 flex items-center justify-center text-sm opacity-50 image-load-error">Fix your internet</p>
+          </div>
+          <div class="h-24  flex gap-2 w-72 small-image-container overflow-scroll">
+            <div class="h-16 aspect-square bg-black"></div>
+            <div class="h-16 aspect-square bg-black"></div>
+            <div class="h-16 aspect-square bg-black"></div>
+            <div class="h-16 aspect-square bg-black"></div>
+            <div class="h-16 aspect-square bg-black"></div>
+            <div class="h-16 aspect-square bg-black"></div>
+            <div class="h-16 aspect-square bg-black"></div>
+          </div>
+        </div>
+        <div class="flex flex-col gap-4 pr-8 w-96">
+          <div>
+            <div class="text-2xl product-name">Essence Mascara Lash Princess</div>
+            <div class="text-sm opacity-75 product-return-policy">30 days return policy</div>
+          </div>
+          <div class="flex items-baseline gap-8">
+            <span class="text-4xl text-red-800 product-price-before-discount">$200</span>
+            <span class="font-bold product-discount">50% off</span>
+            <span class="text-4xl line-through opacity-60 product-price-after-discount">400</span>
+          </div>
+          <div class="text-lg product-stock-left">Only 5 left</div>
+          <div class="text-sm product-description">The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.</div>
+          <div class="flex items-end justify-end w-full h-full">
+            <button class="px-12 py-4 bg-red-900 rounded-md w-fit h-fit text-red-50">Add To Cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex gap-12 flex-col">
+      <div class="flex flex-col gap-6">
+        <h4 class="text-xl">Additional Information</h4>
+        <div class="flex flex-col gap-4 pl-12 text-sm">
+          <div class="flex gap-6">
+            <div>Brand</div>
+            <div class="product-brand">...</div>
+          </div>
+          <div class="flex gap-6">
+            <div>Tags</div>
+            <div class="product-tags">...</div>
+          </div>
+          <div class="flex gap-6">
+            <div>Dimension</div>
+            <div>
+              <span>width</span>
+              <span class="product-width">(20)</span>
+            </div>
+            <div>
+              <span>height</span>
+              <span class="product-height">(30)</span>
+            </div>
+            <div>
+              <span>depth</span>
+              <span class="product-depth">(82)</span>
+            </div>
+          </div>
+          <div class="flex gap-6">
+            <span>Warenty</span>
+            <div class="product-warrenty">...</div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h3 class="mb-6 text-2xl">Reviews</h3>
+        <div class="flex flex-col px-12 product-reviews">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
+const productReviewHtml = `
+<div class="flex gap-2 [&:not(:last-child)]:border-b-2 w-1/2 [&:not(:first-child)]:pt-4 [&:not(:last-child)]:pb-4 product-review">
+  <div class="w-10 h-10 rounded-sm bg-slate-500"></div>
+  <div class="">
+    <div class="flex flex-col mb-2">
+      <span class="product-reviewer-name">Natalie Harris</span>
+      <span class="text-xs opacity-60 product-reviewed-time">1 Min ago</span>
+    </div>
+    <div class="mb-4">
+      <div class="relative flex h-full text-sm text-gray-300">
+        <div class="absolute inset-0 w-[70%] overflow-hidden text-yellow-500 ratings-star product-reviewed-star">
+          <div class="flex w-fit">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 fill-yellow-500" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m12 17.75l-6.172 3.245l1.179-6.873l-5-4.867l6.9-1l3.086-6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 fill-yellow-500" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m12 17.75l-6.172 3.245l1.179-6.873l-5-4.867l6.9-1l3.086-6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 fill-yellow-500" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m12 17.75l-6.172 3.245l1.179-6.873l-5-4.867l6.9-1l3.086-6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 fill-yellow-500" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m12 17.75l-6.172 3.245l1.179-6.873l-5-4.867l6.9-1l3.086-6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+            </svg>
+            <svg class="h-4 fill-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m12 17.75l-6.172 3.245l1.179-6.873l-5-4.867l6.9-1l3.086-6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+            </svg>
+          </div>
+        </div>
+        <div class="flex w-fit">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 fill-transparent" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m12 17.75l-6.172 3.245l1.179-6.873l-5-4.867l6.9-1l3.086-6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 fill-transparent" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m12 17.75l-6.172 3.245l1.179-6.873l-5-4.867l6.9-1l3.086-6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 fill-transparent" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m12 17.75l-6.172 3.245l1.179-6.873l-5-4.867l6.9-1l3.086-6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 fill-transparent" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m12 17.75l-6.172 3.245l1.179-6.873l-5-4.867l6.9-1l3.086-6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 fill-transparent" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m12 17.75l-6.172 3.245l1.179-6.873l-5-4.867l6.9-1l3.086-6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+          </svg>
+        </div>
+      </div>
+    </div>
+    <div class="text-sm product-reviewed-comment">Excellent quality!</div>
+  </div>
+</div>
+`;
+
 const categorySearchEl = document.querySelector("#categorySearchEl");
+const productDescriptionContainer = document.querySelector(
+  ".product-description"
+);
 
 // Dynamically Import categories
 fetch("https://dummyjson.com/products/categories")
@@ -237,11 +375,103 @@ searchForm.addEventListener("submit", async (e) => {
   searchInputEl.value = "";
 });
 
-cardContainer.addEventListener("click", (e) => {
-  console.log();
+cardContainer.addEventListener("click", async (e) => {
+  const id = e.target.closest(".cardBox")?.dataset?.productContainerId;
 
-  console.log(e.target.closest(".cardBox").dataset.productContainerId);
-  fetch("https://dummyjson.com/products/1")
-    .then((res) => res.json())
-    .then(console.log);
+  if (!id) {
+    if (productDescriptionContainer.firstChild) {
+      productDescriptionContainer.removeChild(
+        productDescriptionContainer.firstChild
+      );
+    }
+    return;
+  }
+
+  const productDescriptionDOM = parser.parseFromString(
+    productDescriptionHtml,
+    "text/html"
+  );
+
+  const productDescriptionModel = productDescriptionDOM.querySelector(
+    ".product-description-model"
+  );
+  const productName = productDescriptionModel.querySelector(".product-name");
+  const productReturnPolicy = productDescriptionModel.querySelector(
+    ".product-return-policy"
+  );
+  const productPriceBeforeDiscount = productDescriptionModel.querySelector(
+    ".product-price-before-discount"
+  );
+  const productDiscount =
+    productDescriptionModel.querySelector(".product-discount");
+  const productPriceAfterDiscount = productDescriptionModel.querySelector(
+    ".product-price-after-discount"
+  );
+  const productDescription = productDescriptionModel.querySelector(
+    ".product-description"
+  );
+  const productStockLeft = productDescriptionModel.querySelector(
+    ".product-stock-left"
+  );
+  const productBrand = productDescriptionModel.querySelector(".product-brand");
+  const productTags = productDescriptionModel.querySelector(".product-tags");
+  const productWidth = productDescriptionModel.querySelector(".product-width");
+  const productHeight =
+    productDescriptionModel.querySelector(".product-height");
+  const productDepth = productDescriptionModel.querySelector(".product-depth");
+  const productWarrenty =
+    productDescriptionModel.querySelector(".product-warrenty");
+
+  const data = await fetch(`https://dummyjson.com/products/${id}`).then((res) =>
+    res.json()
+  );
+  console.log(data);
+  productName.textContent = data.title;
+  productReturnPolicy.textContent = data.returnPolicy;
+  productPriceBeforeDiscount.textContent = data.price;
+  productDiscount.textContent = `${data.discountPercentage}%`;
+  productPriceAfterDiscount.textContent = (
+    (data.discountPercentage / 100) *
+    data.price
+  ).toFixed(2);
+  productDescription.textContent = data.description;
+  productStockLeft.textContent = `Only ${data.stock} available`;
+
+  productBrand.textContent = data.brand;
+  productTags.textContent = `${data.tags
+    .map((element) => `#${element}`)
+    .join(" ")}`;
+  productWidth.textContent = `(${data.dimensions.width})`;
+  productHeight.textContent = `(${data.dimensions.height})`;
+  productDepth.textContent = `(${data.dimensions.depth})`;
+  productWarrenty.textContent = data.warrantyInformation;
+
+  for (const review of data.reviews) {
+    const productReviews =
+      productDescriptionModel.querySelector(".product-reviews");
+
+    const productReview = parser
+      .parseFromString(productReviewHtml, "text/html")
+      .querySelector(".product-review");
+
+    const productReviewerName = productReview.querySelector(
+      ".product-reviewer-name"
+    );
+    const productReviewedTime = productReview.querySelector(
+      ".product-reviewed-time"
+    );
+    const productReviewedStar = productReview.querySelector(
+      ".product-reviewed-star"
+    );
+    const productReviewedComment = productReview.querySelector(
+      ".product-reviewed-comment"
+    );
+
+    productReviewerName.textContent = review.reviewerName;
+    productReviewedComment.textContent = review.comment;
+    productReviewedStar.style.width = (review.rating / 5) * 100;
+    productReviewedTime.textContent = timeago.format(review.date);
+    productReviews.appendChild(productReview);
+  }
+  productDescriptionContainer.appendChild(productDescriptionModel);
 });
